5a3136267e32691a6338c2016e4e96d48720164e
farooq8f <95857826+farooq8f@users.noreply.github.com>
2021-12-09 03:53:19 -0800

    Add files via upload

---
+++ f.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  
+  <!--this link for social icon-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="css/socialstyle.css">
+<!--social icon links end-->
+
+<!--this link for dropdown icon-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+ 
+<!--dropdown link end-->
+
+</head>
+<body>
+    
+<!--Social media icon-->
+
+<div class="container flex-row-reverse">
+    <div class="row"> 
+      <div>
+        <ul class="social-network social-circle"> 
+		    <li><a href="https://www.google.com/maps/place/govt.+postgraduate+college+timergara/@34.8453254,71.8297424,15z/data=!4m5!3m4!1s0x0:0x971eda3357c2e933!8m2!3d34.8453254!4d71.8297424" target="_blank" class="icoLinkedin" title="Find us"><i class="fa fa-map-marker"></i></a></li>  
+			<li><a href="https://web.facebook.com/gpgctmg/?_rdc=1&_rdr" target="_blank" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+			<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+		 
+		   
+            
+            <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+             <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+
+
+
+        </ul>
+      </div>
+    </div>
+  </div> 
+<!--End of Social media icon-->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+ 
+
+	<link rel="stylesheet" href="style.css"> 
+	<script src="script.js"></script>
+</head>
+<body>
+	
+
+<!-- Navbar -->
+<nav class="navbar fixed-top navbar-expand-sm  scrolling-navbar">
+    <div class="container">
+
+      <!-- Brand -->
+      <a class="navbar-brand" href="#" target="_blank">
+        <strong>Pakjobs</strong>
+      </a>
+
+      <!-- Collapse -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+     
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Left -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+            
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" target="_blank">kpk jobs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" target="_blank">Isb jobs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" target="_blank">Sindh Jobs</a>
+          </li>
+        </ul>
+
+         
+        </ul>
+
+      </div>
+      
+
+    </div>
+    
+  </nav>
+  
+  <!-- Navbar -->
+
+  <!-- Full Page Intro -->
+  <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/93.jpg'); background-repeat: no-repeat; background-size: cover;">
+
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+      <!-- Content -->
+    
+      <!-- Content -->
+
+    </div>
+    <!-- Mask & flexbox options-->
+
+  </div>
+  <!-- Full Page Intro -->
+
+  
+
+
+  
+
+        </div>
+
+      </section>
+
+<div class="container">
+  <div class="row">
+    <div class="col-4">
+    <div class="card" style="width: 18rem;">
+  <img src="khan2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="khan2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="khan2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="khan2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+    </div>
+  </div>
+</div> 
+
+</body>
+</html>
+      
